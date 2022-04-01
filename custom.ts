4658c392fc3aebd29198116f4e021e478cc8d8cc
learnabouts2021 <102753663+learnabouts2021@users.noreply.github.com>
2022-04-01 23:22:49 +0700

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,87 @@
+
+/**
+* Use this file to define custom functions and blocks.
+* Read more at https://makecode.microbit.org/blocks/custom
+*/
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace learn_about_motor {
+    export enum MotorDirection {
+        //% block="forward"
+        Forward,
+        //% block="reverse"
+        Reverse
+    }
+    export enum Motors {
+        //%blockId=learn_about_motor_one
+        //% block="motor 1"
+        Motor1,
+        //%blockId=learn_about_motor_two
+        //% block="motor 2"
+        Motor2
+    }
+    //% blockId=learn_about_motordriver_motor_on
+    //% block="%motor|on direction %dir|speed %speed"
+    //% speed.min=0 speed.max=100
+    export function motorOn(motor: Motors, dir: MotorDirection, speed: number): void {
+        /*first convert 0-100 to 0-1024 (approx) We wont worry about the lsat 24 to make life simpler*/
+        let OutputVal = Math.clamp(0, 100, speed) * 10;
+
+        switch (motor) {
+            case Motors.Motor1: /*Motor 1 uses Pins 13 and 14*/
+                switch (dir) {
+                    case MotorDirection.Forward:
+                        pins.analogWritePin(AnalogPin.P13, OutputVal);
+                        pins.digitalWritePin(DigitalPin.P14, 0); /*Write the low side digitally, to allow the 3rd PWM to be used if required elsewhere*/
+                        break
+                    case MotorDirection.Reverse:
+                        pins.analogWritePin(AnalogPin.P14, OutputVal);
+                        pins.digitalWritePin(DigitalPin.P13, 0);
+                        break
+                }
+
+                break;
+            case Motors.Motor2: /*Motor 2 uses Pins 15 and 16*/
+                switch (dir) {
+                    case MotorDirection.Forward:
+                        pins.analogWritePin(AnalogPin.P15, OutputVal);
+                        pins.digitalWritePin(DigitalPin.P16, 0); /*Write the low side digitally, to allow the 3rd PWM to be used if required elsewhere*/
+                        break
+                    case MotorDirection.Reverse:
+                        pins.analogWritePin(AnalogPin.P16, OutputVal);
+                        pins.digitalWritePin(DigitalPin.P15, 0);
+                        break
+                }
+
+                break;
+        }
+    }
+    /**
+     * Turns off the motor specified by eMotors
+     * @param motor :which motor to turn off
+     */
+    //% blockId=learn_about_motor_off
+    //%block="turn off %motor"
+    export function motorOff(motor: Motors): void {
+        switch (motor) {
+            case Motors.Motor1:
+                pins.digitalWritePin(DigitalPin.P13, 0);
+                pins.digitalWritePin(DigitalPin.P14, 0);
+                break
+            case Motors.Motor2:
+                pins.digitalWritePin(DigitalPin.P15, 0);
+                pins.digitalWritePin(DigitalPin.P16, 0);
+                break
+        }
+    }
+}
---
+++ main.ts
@@ -0,0 +1,3 @@
+basic.forever(function () {
+    learn_about_motor.motorOn(learn_about_motor.Motors.Motor1, learn_about_motor.MotorDirection.Forward, 0)
+})
